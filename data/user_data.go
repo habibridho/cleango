cb35e1e9b3d3b4956c45502f4e6cc9e7626ae335
Habib Ridho <habib.ridho@amartha.com>
2019-03-11 01:05:01 +0700

    implement user data

---
+++ data/user_data.go
@@ -0,0 +1,38 @@
+package data
+
+import (
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/habibridho/cleango/entities"
+)
+
+type UserDataImpl struct {
+	MgSession mgo.Session
+}
+
+type User struct {
+	ID       uint64 `bson:"id"`
+	Username string `bson:"username"`
+	Fullname string `bson:"fullname"`
+	Password string `bson:"password"`
+}
+
+func (impl UserDataImpl) GetUser(username string) (user entities.User, err error) {
+	query := bson.M{
+		"username": username,
+	}
+
+	result := User{}
+	if err = impl.MgSession.DB("user").C("user").Find(query).One(&result); err != nil {
+		return
+	}
+
+	user = entities.User{
+		ID:       result.ID,
+		Username: result.Username,
+		Fullname: result.Fullname,
+		Password: result.Password,
+	}
+
+	return
+}
