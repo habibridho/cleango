2ce27438f589e0d508d03aa444814545db146409
Habib Ridho <habib.ridho@amartha.com>
2019-03-10 12:05:22 +0700

    add entities

---
+++ entities/movie.go
@@ -0,0 +1,7 @@
+package entities
+
+type Movie struct {
+	ID       uint64
+	Title    string
+	Director string
+}
---
+++ entities/user.go
@@ -0,0 +1,12 @@
+package entities
+
+type User struct {
+	ID       uint64
+	Username string
+	Fullname string
+	Password string
+}
+
+func (u User) SetFullname(firstName string, lastName string) {
+	u.Fullname = firstName + " " + lastName
+}
